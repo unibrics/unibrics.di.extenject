4c6df5b00f12ad14c018d4241cc370dbce3b4cd1
artem.korotenko <artem.korotenko@betterme.world>
2021-04-22 02:26:06 +0300

    configuring save converter

--- Runtime/General/ExtenjectService.cs
+++ Runtime/General/ExtenjectService.cs
@@ -2,60 +2,76 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Core.DI;
     using Core.Services;
+    using Tools;
+    using UnityEngine;
     using Zenject;
     using Zenject.Internal;
     using InjectAttribute = Core.DI.InjectAttribute;
 
     class ExtenjectService : IDependencyInjectionService
     {
-        public IInstanceProvider InstanceProvider => new ExtenjectWrapper(diContainer);
-        
+        private readonly ExtenjectWrapper wrapper;
+
         private readonly DiContainer diContainer;
+
+        public IResolver Resolver => wrapper;
 
         private readonly List<ServiceDescriptor> descriptors = new List<ServiceDescriptor>();
 
+        public IInstanceProvider InstanceProvider => wrapper;
+        
         public ExtenjectService(DiContainer diContainer)
         {
             this.diContainer = diContainer;
+            wrapper = new ExtenjectWrapper(diContainer);
             ReflectionTypeAnalyzer.AddCustomInjectAttribute<InjectAttribute>();
         }
         
-        public void PrepareServices()
+        public void Validate()
+        {
+            foreach (var descriptor in descriptors.Where(descriptor => !descriptor.IsConfirmed))
+            {
+                throw new UnibricsException(
+                    $"Descriptor for types {string.Join(",", descriptor.InterfaceTypes.Select(type => type.Name))} " +
+                    $"was not confirmed. Probably, you forget to set scope");
+            }
+        }
+
+        public void PrepareService(ServiceDescriptor descriptor)
         {
             try
             {
-                foreach (var descriptor in descriptors)
+                descriptor.Validate();
+                var binding = diContainer.Bind(descriptor.InterfaceTypes);
+                if (descriptor.ImplementationObject != null)
                 {
-                    descriptor.Validate();
-                    var binding = diContainer.Bind(descriptor.InterfaceTypes);
-                    if (descriptor.ImplementationObject != null)
-                    {
-                        binding.FromInstance(descriptor.ImplementationObject);
-                        continue;
-                    }
+                    binding.FromInstance(descriptor.ImplementationObject);
+                    return;
+                }
 
-                    var from = binding.To(descriptor.ImplementationType);
-                    if (descriptor.Scope == ServiceScope.Singleton)
-                    {
-                        from.AsSingle();
-                    }
-                    else
-                    {
-                        from.AsTransient();
-                    }
+                var from = binding.To(descriptor.ImplementationType);
+                if (descriptor.Scope == ServiceScope.Singleton)
+                {
+                    from.AsSingle();
+                }
+                else
+                {
+                    from.AsTransient();
                 }
             }
             catch (Exception e)
             {
-                throw new Exception($"Exception occured during Extenject DI setup:", e);
+                throw new Exception($"Exception occured during Extenject DI service setup:", e);
             }
         }
 
         public void Add(ServiceDescriptor descriptor)
         {
             descriptors.Add(descriptor);
+            descriptor.OnDescriptorFinalized(() => PrepareService(descriptor));
         }
     }
 }
